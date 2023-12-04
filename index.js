cc93c380e8198ed0bc24fa9733bc6dfa15608e34
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-12-04 18:01:20 +0200

    cor

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 app.use(cors({
-    
     origin: [
-      "https://portfolio-builder-frontend.vercel.app/",
+      "https://portfolio-builder-frontend.vercel.app",
+      "http://localhost:3000"
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
