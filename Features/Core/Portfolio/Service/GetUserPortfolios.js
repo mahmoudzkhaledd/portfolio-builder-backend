ce6a02b223e864ead7f31aaff2819dbf4c888fca
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-12-09 10:20:30 +0200

    change views

--- Features/Core/Portfolio/Service/GetUserPortfolios.js
+++ Features/Core/Portfolio/Service/GetUserPortfolios.js
@@ -13,7 +13,7 @@ function numOfDate(firstDate, secondDate) {
     return diffDays
 }
 
-function addView(viewsHistory, initial) {
+function addView(viewsHistory, increase) {
     const dateNow = new Date();
     if (viewsHistory.length == 0) {
         for (let i = 0; i < 30; i++) {
@@ -23,9 +23,9 @@ function addView(viewsHistory, initial) {
             };
             viewsHistory.push(obj);
         }
-        viewsHistory[viewsHistory.length - 1].views = initial;
+        viewsHistory[viewsHistory.length - 1].views += increase;
     } else if (dateNow.toLocaleDateString() == viewsHistory[viewsHistory.length - 1].date) {
-        viewsHistory[viewsHistory.length - 1].views++;
+        viewsHistory[viewsHistory.length - 1].views += increase;
     } else if (numOfDate(dateNow, viewsHistory[viewsHistory.length - 1].date) <= 30) {
         const diff = numOfDate(dateNow, viewsHistory[viewsHistory.length - 1].date);
         for (let i = 1; i <= diff; i++) {
@@ -35,7 +35,7 @@ function addView(viewsHistory, initial) {
             };
             viewsHistory.push(obj);
         }
-        viewsHistory[viewsHistory.length - 1].views++;
+        viewsHistory[viewsHistory.length - 1].views += increase;
         viewsHistory.splice(0, diff);
     }
     return viewsHistory;
@@ -64,10 +64,11 @@ exports.getPortfolio = asyncHandeler(async (req, res, next) => {
     if (port == null || (userModel == null && !port.online) || (!port.online && userModel != null && userModel.id != port.userId)) {
         return res.sendStatus(404);
     }
+    port.viewsHistory = addView(port.viewsHistory, 0);
     if (userModel == null || port.userId != userModel.id) {
         port.totalViews++;
         port.viewsHistory = port.viewsHistory || [];
-        port.viewsHistory = addView(port.viewsHistory, port.totalViews);
+        port.viewsHistory = addView(port.viewsHistory, port.viewsHistory.length == 0 ? port.totalViews : 1);
         await port.save();
     }
     res.status(200).json({
